659f83de11ad04e770f967845556041af7f17980
Alanmuro77 <alanmuro77@gmail.com>
2025-02-21 16:30:57 -0600

    Funcion para agregar amigos

--- app.js
+++ app.js
@@ -2,3 +2,16 @@
 
 
 let friends = [];
+
+
+
+// funcion para agregar amigos
+
+let capture = document.getElementById('amigo').value; // capturamos lo que ingrese el usuario
+
+if (capture === ''){
+    alert("Please, enter a name");
+} else{
+    friends.push(capture);
+    capture.getElementById('amigo').value = ''; // limpiamos el campo de entrada
+}
